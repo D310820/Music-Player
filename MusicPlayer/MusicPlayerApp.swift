94c1cb80bb78551ae5366605466775ac23072150
D310820 <tecnocel30210@gmail.com>
2026-05-24 18:06:40 -0600

    Initial Commit

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Jose Daniel Espinoza Gomez on 22/05/26.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
